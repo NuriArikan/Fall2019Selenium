9e84c3a6a5e1fc6daeed064fb8fb12a205877538
NuriArikan <nuri@cornelldecor.com>
2020-03-04 21:58:11 -0500

    Basic Navigation

--- src/test/java/com/automation/utilities/DriverFactory.java
+++ src/test/java/com/automation/utilities/DriverFactory.java
@@ -3,27 +3,29 @@ package com.automation.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 public class DriverFactory {
-
-    /**
-     * You homework will be to improve this method!
-     * This method return webdriver object based on browser type
-     * If you want to use chrome browser, just provide chrome as a parameter
-     * @param browserName
-     * @return webdriver object
-     */
-
-
-    public static WebDriver createADriver( String browserName){
-
-        if (browserName.equalsIgnoreCase("chrome")){
+    public static WebDriver createDriver(String browserName){
+        if (browserName.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
             return new ChromeDriver();
-        }else {
+        } else if (browserName.equalsIgnoreCase("firefox")){
             WebDriverManager.firefoxdriver().setup();
-            return  new FirefoxDriver();
+            return new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            return new EdgeDriver();
+        } else if (browserName.equalsIgnoreCase("ie")) {
+            WebDriverManager.iedriver().setup();
+            return new InternetExplorerDriver();
+        } else if (browserName.equalsIgnoreCase("safari")) {
+            return new SafariDriver();
         }
+        return null;
     }
+
+
 }
